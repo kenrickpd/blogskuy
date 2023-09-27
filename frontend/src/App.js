93c973854fbf5fc81493903cf2deeaf94f1b8270
raaizar <rahmanraiza@gmail.com>
2023-09-27 12:07:49 +0700

    feat: register page done

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -2,6 +2,7 @@ import React from 'react'
 import { BrowserRouter, Routes, Route } from 'react-router-dom'
 import LandingPage from './pages/landing-page'
 import LoginPage from './pages/login-page'
+import RegisterPage from './pages/register-page'
 
 function App() {
   return (
@@ -9,6 +10,7 @@ function App() {
       <Routes>
         <Route index element={<LandingPage />} />
         <Route path="/login" element={<LoginPage />} />
+        <Route path='/register' element={<RegisterPage />} />
       </Routes>
     </BrowserRouter>
   )
---
+++ frontend/src/pages/register-page.jsx
@@ -0,0 +1,30 @@
+import React from 'react'
+import sally from '../assets/saly.png'
+import BlackButton from '../components/button/button'
+import InputField from '../components/input-field/input-field'
+
+export default function RegisterPage() {
+  return (
+    <div>
+      <div className="flex w-screen h-12 px-16 py-24 justify-center items-center">
+        <p className="text-2xl md:text-3xl lg:text-4xl xl:text-5xl">
+          Blogskuy Register
+        </p>
+      </div>
+      <div className="block lg:flex xl:flex px-16 lg:space-x-28 xl:space-x-28 justify-center items-center">
+        <div className="flex justify-center items-center">
+          <img src={sally} width={500} height={500} />
+        </div>
+        <div className="text-2xl flex justify-center items-center">
+          <div className="block space-y-4">
+            <InputField inputType="text">Full Name</InputField>
+            <InputField inputType="text">Email</InputField>
+            <InputField inputType="password">Password</InputField>
+            <InputField inputType="password">Confirm Password</InputField>
+            <BlackButton>Register</BlackButton>
+          </div>
+        </div>
+      </div>
+    </div>
+  )
+}
